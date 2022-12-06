3bf3d315f4389baef229e5fc5471fe4dd3bb2d49
bywhite <bywhite@cisco.com>
2022-12-06 16:53:30 -0600

    added thermal policy to chassis

--- imm-domain-fabric-module/chassis-policies.tf
+++ imm-domain-fabric-module/chassis-policies.tf
@@ -2,7 +2,7 @@
 
 
 resource "intersight_access_policy" "chassis_9508_access1" {
-  name        = "${var.policy_prefix}-imc-access-chassis-policy-1"
+  name        = "${var.policy_prefix}-chassis-imc-access-policy-1"
   description = var.description
   inband_vlan = var.chassis_imc_access_vlan
   inband_ip_pool {
@@ -29,18 +29,41 @@ resource "intersight_access_policy" "chassis_9508_access1" {
   }
 }
 
-# Need Power Policy for 9508
 
 # Need Thermal Policy for 9508
-
 # Need to Consume an SNMP policy
 
 resource "intersight_power_policy" "chassis_9508_power1" {
-  name        = "${var.policy_prefix}-power-chassis-policy-1"
+  name        = "${var.policy_prefix}-chassis-power-policy-1"
   description              = var.description
   power_save_mode = "Enabled"
   dynamic_rebalancing = "Enabled"
   extended_power_capacity = "Disabled"
+
+  organization {
+    moid        = var.organization
+    object_type = "organization.Organization"
+  }
+
+  # assign this policy to the chassis profile being created
+  profiles {
+    moid        = intersight_chassis_profile.chassis_9508_profile1.moid
+    object_type = "chassis.Profile"
+  }
+  
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+
+resource "intersight_thermal_policy" "chassis_9508_thermal1" {
+  name        = "${var.policy_prefix}-chassis-thermal-policy-1"
+  description              = var.description
+  fan_control_mode = "Balanced"
 
   organization {
     moid        = var.organization
