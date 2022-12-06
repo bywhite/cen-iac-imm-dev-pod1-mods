8a12c0176964a377d71095d856bcdbed7124c06a
bywhite <bywhite@cisco.com>
2022-12-06 16:35:43 -0600

    Added PowerPolicy to Chassis_9508 Profile

--- imm-domain-fabric-module/chassis-policies.tf
+++ imm-domain-fabric-module/chassis-policies.tf
@@ -29,3 +29,29 @@ resource "intersight_access_policy" "chassis_9508_access1" {
 
 # Need to Consume an SNMP policy
 
+resource "intersight_power_policy" "chassis_9508_power1" {
+  name        = "${var.policy_prefix}-power-chassis-policy-1"
+  description              = var.description
+  power_save_mode = "Enabled"
+  dynamic_rebalancing = "Enabled"
+  extended_power_capacity = "Disabled"
+
+  organization {
+    moid        = var.organization
+    object_type = "organization.Organization"
+  }
+
+  # assign this policy to the chassis profile being created
+  profiles {
+    moid        = intersight_chassis_profile.chassis_9508_profile1.moid
+    object_type = "chassis.Profile"
+  }
+  
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
