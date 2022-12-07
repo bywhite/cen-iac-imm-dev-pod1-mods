16d71aabb34b4eda2ad63022b1229b247c3cb6ee
bywhite <bywhite@cisco.com>
2022-12-07 14:51:56 -0600

    QoS updated for 6536 domain CoS

--- imm-domain-fabric-6536/fi-policies.tf
+++ imm-domain-fabric-6536/fi-policies.tf
@@ -86,7 +86,8 @@ resource "intersight_networkconfig_policy" "connectivity1" {
 # System Qos Policy
 # -----------------------------------------------------------------------------
 
-# This will create the default System QoS policy with zero customization
+# This will create the default System QoS policy with some generic settings
+# FlexPod: https://www.cisco.com/c/en/us/td/docs/unified_computing/ucs/UCS_CVDs/flexpod_datacenter_vmware_netappaffa.html
 # Needs customization for vNics to change from best effort with MTU 1500 to MTU 9216 and higher priority
 resource "intersight_fabric_system_qos_policy" "qos1" {
   name        = "${var.policy_prefix}-system-qos-policy-1"
@@ -97,17 +98,31 @@ resource "intersight_fabric_system_qos_policy" "qos1" {
     object_type = "organization.Organization"
   }
   
+
+  classes {
+    admin_state        = "Enabled"
+    bandwidth_percent  = 14
+    weight             = 5
+    cos                = 255
+    mtu                = 1500
+    multicast_optimize = false
+    name               = "Best Effort"
+    packet_drop        = true
+    class_id           = "fabric.QosClass"
+    object_type        = "fabric.QosClass"    
+  }
+
   classes {
     admin_state        = "Enabled"
     bandwidth_percent  = 20
+    weight             = 7     
     cos                = 1
     mtu                = 1500
     multicast_optimize = false
     name               = "Bronze"
     packet_drop        = true
     class_id           = "fabric.QosClass"
-    object_type        = "fabric.QosClass"
-    weight             = 7       
+    object_type        = "fabric.QosClass"  
   }
 
 
@@ -119,6 +134,47 @@ resource "intersight_fabric_system_qos_policy" "qos1" {
     mtu                = 9216
     multicast_optimize = false
     name               = "Silver"
+    packet_drop        = true
+    class_id           = "fabric.QosClass"
+    object_type        = "fabric.QosClass"    
+  }
+  
+  # Class of Service 3 is used for FibreChannel (fcoe)
+  classes {
+    admin_state        = "Enabled"
+    bandwidth_percent  = 17
+    weight             = 5
+    cos                = 3
+    mtu                = 2240
+    multicast_optimize = false
+    name               = "FC"
+    packet_drop        = false
+    class_id           = "fabric.QosClass"
+    object_type        = "fabric.QosClass"    
+  }
+  
+
+  classes {
+    admin_state        = "Enabled"
+    bandwidth_percent  = 26
+    weight             = 9
+    cos                = 4
+    mtu                = 9216
+    multicast_optimize = false
+    name               = "Gold"
+    packet_drop        = true
+    class_id           = "fabric.QosClass"
+    object_type        = "fabric.QosClass"    
+  }
+  
+  classes {
+    admin_state        = "Disabled"
+    bandwidth_percent  = 0
+    weight             = 10
+    cos                = 5
+    mtu                = 9216
+    multicast_optimize = false
+    name               = "Platinum"
     packet_drop        = true
     class_id           = "fabric.QosClass"
     object_type        = "fabric.QosClass"    
