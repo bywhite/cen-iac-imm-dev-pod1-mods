015c446bbcda0045d05ecd693302ab171858c871
bywhite <bywhite@cisco.com>
2022-12-07 17:54:35 -0600

    Removed unused native vlan variable

--- imm-domain-fabric-6536/variables.tf
+++ imm-domain-fabric-6536/variables.tf
@@ -1,5 +1,5 @@
 # =============================================================================
-# External references
+# Org external references
 # -----------------------------------------------------------------------------
 
 variable "organization" {
@@ -7,24 +7,6 @@ variable "organization" {
   description = "moid for organization in which to create the policies"
 }
 
-
-# variable "wwnn-block" {
-#   type         = string
-#   description  = "beginning WWNN block of size 255"
-#   default      = "20:00:00:CA:FE:00:00:01"
-# }
-
-# variable "wwpn-a-block" {
-#   type         = string
-#   description  = "beginning WWPN-A block of size 255"
-#   default      = "20:00:00:CA:FE:0A:00:01"
-# }
-
-# variable "wwpn-b-block" {
-#   type         = string
-#   description  = "beginning WWPN-B block of size 255"
-#   default      = "20:00:00:CA:FE:0B:00:01"
-# }
 
 # =============================================================================
 # Naming and tagging
@@ -35,14 +17,6 @@ variable "policy_prefix" {
   description = "prefix for all policies created"
   default     = "tf"
 }
-
-### domain_instances are NOT USED when creating 1 domain per module call, to increases customization
-# variable "domain_instances" {
-#   type        = list(string)
-#   description = "list of domain instances to be created [vmw-1,lnx-1,bml-1,vmw-2]"
-#   default     = ["vmw-1"]
-#   # Each domain-instance is concatenated with the policy_prefix to Name the domain
-# }
 
 variable "description" {
   type        = string
@@ -55,31 +29,6 @@ variable "tags" {
   default     = []
 }
 
-
-# =============================================================================
-# Fabric Interconnect 6454 ports and VLANs
-# -----------------------------------------------------------------------------
-
-# variable "server_ports_6454" {
-#   type        = set(string)
-#   description = "list of port numbers to be assigned to server ports"
-# }
-# variable "port_channel_6454" {
-#   type        = set(string)
-#   description = "list of port numbers to be assigned to uplink port channel"
-# }
-# variable "fc_port_count_6454" {
-#   type        = number
-#   description = "number of ports to assign to FC starting at port 35"
-# }
-# variable "uplink_vlans_6454" {
-#   type        = map(number)
-#   description = "map of vlan names and IDs to be used on FI uplinks"
-# }
-# variable "vnic_native_vlan" {
-#   type        = number
-#   description = "native VLAN for vnic profiles"
-# }
 
 # =============================================================================
 # Fabric Interconnect 6536 ports and VLANs
@@ -101,25 +50,7 @@ variable "uplink_vlans_6536" {
   type        = map(number)
   description = "map of vlan names and IDs to be used on FI uplinks"
 }
-variable "vnic_native_vlan_6536" {
-  type        = number
-  description = "native VLAN for vnic profiles"
-}
 
-
-# =============================================================================
-# IMC for Servers
-# -----------------------------------------------------------------------------
-
-# variable "imc_access_vlan" {
-#   type        = number
-#   description = "ID of VLAN for IMC access"
-# }
-# variable "imc_admin_password" {
-#   type        = string
-#   description = "password for the local user policy for IMC"
-#   default     = "Cha@ng3Me"
-# }
 
 # =============================================================================
 # Chassis Access - IMC
@@ -129,42 +60,11 @@ variable "chassis_imc_access_vlan" {
   type        = number
   description = "ID of VLAN for Chassis In-Band IMC access"
 }
-
 variable "chassis_imc_ip_pool_moid" {
   type = string
   description = "moid of Chassis IP_Pool to be assigned to IMC Access Policy"
 }
 
-
-
-# =============================================================================
-# Pools for Server Configs
-# -----------------------------------------------------------------------------
-
-# variable "mac_pool_moid" {
-#   type         = string
-#   description = "MAC Pool MOID"
-# } 
-
-# variable "imc_ip_pool_moid" {
-#   type = string
-#   description = "moid of IP_Pool to be assigned to IMC Access Policy"
-# }
-
-# variable "wwnn_pool_moid" {
-#   type = string
-#   description = "moid of WWNN Pool"
-# }
-
-# variable "wwpn_pool_a_moid" {
-#   type = string
-#   description = "moid of WWPN A fabric Pool"
-# }
-
-# variable "wwpn_pool_b_moid" {
-#   type = string
-#   description = "moid of WWPN B fabric Pool"
-# }
 
 
 # =============================================================================
