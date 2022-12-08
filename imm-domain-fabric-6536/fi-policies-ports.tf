3e586e8cdd1a30324302a3cab8ede129b584054a
bywhite <bywhite@cisco.com>
2022-12-07 21:31:54 -0600

    set custom_mode   = "BreakoutFibreChannel32G"

--- imm-domain-fabric-6536/fi-policies-ports.tf
+++ imm-domain-fabric-6536/fi-policies-ports.tf
@@ -60,7 +60,9 @@ resource "intersight_fabric_port_policy" "fi6536_port_policy-b" {
 resource "intersight_fabric_port_mode" "fi6536_port_mode1" {
   count = (var.fc_port_count_6536 > 0) ? 1 : 0
 
-  custom_mode   = "FibreChannel"
+  #custom_mode   = "FibreChannel"
+  custom_mode   = "BreakoutFibreChannel32G"
+  
   port_id_end   = 36
   port_id_start = 35
   #port_id_start   = "${36 - var.fc_port_count_6536 + 1}"
