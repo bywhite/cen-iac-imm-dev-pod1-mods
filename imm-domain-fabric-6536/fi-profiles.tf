c21569c4b610279d8554a3fbaa90043bdcea1a2e
bywhite <bywhite@cisco.com>
2022-12-08 09:43:27 -0600

    removed commented code

--- imm-domain-fabric-6536/fi-profiles.tf
+++ imm-domain-fabric-6536/fi-profiles.tf
@@ -25,15 +25,6 @@ resource "intersight_fabric_switch_cluster_profile" "fi6536_cluster_profile" {
     moid        = var.organization
   }
 
-  # switch_profiles {
-  #   moid        = intersight_fabric_switch_profile.fi6536_switch_profile_a.moid
-  #   object_type = "fabric.SwitchProfile"
-  # }
-  # switch_profiles {
-  #   moid        = intersight_fabric_switch_profile.fi6536_switch_profile_b.moid
-  #   object_type = "fabric.SwitchProfile"
-  # }
-
   dynamic "tags" {
     for_each = var.tags
     content {
