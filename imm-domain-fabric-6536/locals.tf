4b2bf988c8eb7e65953e1e4b1f37823907a04069
bywhite <bywhite@cisco.com>
2022-12-09 23:57:53 -0600

    who changed that?

--- imm-domain-fabric-6536/locals.tf
+++ imm-domain-fabric-6536/locals.tf
@@ -1,7 +1,7 @@
 
 locals {
   #chassis_profile_moids = toset(intersight_chassis_profile.chassis_9508_profile[*].moid)
-  chassis_profile_moids = intersight_chassis_profile.chassis_9508_profile[*].config_result[0].moid
+  chassis_profile_moids = intersight_chassis_profile.chassis_9508_profile[*].config_results[0].moid
   
   # Create a list of chassis indexes Example of five chassis: [0,1,2,3,4]
   chassis_index_numbers  = range(var.chassis_9508_count)
