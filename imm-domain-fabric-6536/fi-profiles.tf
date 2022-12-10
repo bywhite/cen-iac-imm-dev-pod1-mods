376c7f38673d2613117051b9049b043ed7898a80
bywhite <bywhite@cisco.com>
2022-12-09 20:49:57 -0600

    Added outputs to vmw-1 module

--- imm-domain-fabric-6536/fi-profiles.tf
+++ imm-domain-fabric-6536/fi-profiles.tf
@@ -12,27 +12,8 @@
 
 
 # =============================================================================
-# 6536 Cluster (Domain) and Switch Profiles
+# 6536 Cluster (Domain) Switch Profiles
 # -----------------------------------------------------------------------------
-
-### FI 6536 DOMAIN Profile  (cluster_profile) ####
-resource "intersight_fabric_switch_cluster_profile" "fi6536_cluster_profile" {
-  name        = "${var.policy_prefix}-Domain"
-  description = var.description
-  type        = "instance"
-  organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
-  }
-
-  dynamic "tags" {
-    for_each = var.tags
-    content {
-      key   = tags.value.key
-      value = tags.value.value
-    }
-  }
-}
 
 
 ### NEW #### 6536 Switch Profile A ####
--- imm-domain-fabric-6536/main.tf
+++ imm-domain-fabric-6536/main.tf
@@ -1,7 +1,25 @@
 
 
-locals {
 
-# Create a list of chassis indexes Example of five chassis: [0,1,2,3,4]
-  chassis_index = range(var.chassis_9508_count)
+# =============================================================================
+# 6536 Cluster (Domain) 
+# -----------------------------------------------------------------------------
+
+### FI 6536 DOMAIN Profile  (cluster_profile) ####
+resource "intersight_fabric_switch_cluster_profile" "fi6536_cluster_profile" {
+  name        = "${var.policy_prefix}-Domain"
+  description = var.description
+  type        = "instance"
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
 }
