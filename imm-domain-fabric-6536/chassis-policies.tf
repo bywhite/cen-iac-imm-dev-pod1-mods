3847763f17f668b2d0165e009685eaaa2afcf0c6
bywhite <bywhite@cisco.com>
2022-12-09 23:01:56 -0600

    pick a number, any number     ... 42

--- imm-domain-fabric-6536/chassis-policies.tf
+++ imm-domain-fabric-6536/chassis-policies.tf
@@ -1,7 +1,8 @@
 # Policies consumed by Chassis Profiles
 
-
-# Assigns IP address to chassis for managment
+# =============================================================================
+# Chassis IP Access Policy
+# -----------------------------------------------------------------------------
 resource "intersight_access_policy" "chassis_9508_access" {
   name        = "${var.policy_prefix}-Chassis-IMC-Access-1"
   description = var.description
@@ -52,7 +53,9 @@ resource "intersight_access_policy" "chassis_9508_access" {
   ]
 }
 
-# Configure Chassis to Grid power and other power related settings
+# =============================================================================
+# Chassis Power Policy
+# -----------------------------------------------------------------------------
 resource "intersight_power_policy" "chassis_9508_power" {
   name        = "${var.policy_prefix}-Chassis-9508-Power-1"
   description              = var.description
@@ -84,7 +87,9 @@ resource "intersight_power_policy" "chassis_9508_power" {
   ]
 }
 
-# Set Chassis fan characteristics and influence over chassis power consumption
+# =============================================================================
+# Thermal Policy
+# -----------------------------------------------------------------------------
 resource "intersight_thermal_policy" "chassis_9508_thermal" {
   name        = "${var.policy_prefix}-Chassis-9508-Thermal-1"
   description              = var.description
@@ -108,6 +113,64 @@ resource "intersight_thermal_policy" "chassis_9508_thermal" {
     }
   }
   depends_on = [
+    intersight_chassis_profile.chassis_9508_profile
+  ]
+}
+
+
+# =============================================================================
+# SNMP Policy
+# -----------------------------------------------------------------------------
+
+resource "intersight_snmp_policy" "snmp1" {
+  name        = "${var.policy_prefix}-SNMP-Policy"
+  description              = var.description
+  enabled                 = true
+  snmp_port               = 161
+  access_community_string = "anythingbutpublic"
+  community_access        = "Disabled"
+  trap_community          = "TrapCommunity"
+  sys_contact             = "The SysAdmin"
+  sys_location            = "The Data Center"
+  engine_id               = "vvb"
+  snmp_users {
+    name         = "snmpuser"
+    privacy_type = "AES"
+    auth_password    = var.snmp_password
+    privacy_password = var.snmp_password
+    security_level = "AuthPriv"
+    auth_type      = "SHA"
+    object_type    = "snmp.User"
+  }
+  snmp_traps {
+    destination = var.snmp_ip
+    enabled     = false
+    port        = 660
+    type        = "Trap"
+    user        = "snmpuser"
+    nr_version  = "V3"
+    object_type = "snmp.Trap"
+  }
+  # dynamic "profiles" {
+  #   for_each = local.chassis_profile_moids
+  #   content {
+  #     moid        = profiles.value
+  #     object_type = "chassis.Profile"
+  #   }
+  # }
+  profiles {
+    moid        = intersight_fabric_switch_profile.fi6536_switch_profile_a.moid
+    object_type = "fabric.SwitchProfile"
+  }
+  profiles {
+    moid        = intersight_fabric_switch_profile.fi6536_switch_profile_b.moid
+    object_type = "fabric.SwitchProfile"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+    depends_on = [
     intersight_chassis_profile.chassis_9508_profile
   ]
 }
