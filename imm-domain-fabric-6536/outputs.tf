072af5cbe5e84870854a7a6be684e1e3cc224843
bywhite <bywhite@cisco.com>
2022-12-09 21:04:58 -0600

    added chassis profiles output

--- imm-domain-fabric-6536/outputs.tf
+++ imm-domain-fabric-6536/outputs.tf
@@ -12,3 +12,8 @@ output "fi6536_cluster_profile_moid" {
   description = "moid of domain cluster profile created"
   value       = intersight_fabric_switch_cluster_profile.fi6536_cluster_profile.moid
 }
+
+output "chassis_9508_profiles" {
+  description = "Chassis cluster profiles created"
+  value       = intersight_chassis_profile.chassis_9508_profile
+}
