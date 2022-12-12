d6e854f23640b358c554581f3f7f6534bab91007
bywhite <bywhite@cisco.com>
2022-12-12 15:49:44 -0600

    added server count variable

--- imm-pod-servers-mod/vlans.tf
+++ imm-pod-servers-mod/vlans.tf
@@ -22,8 +22,13 @@ resource "intersight_fabric_eth_network_policy" "fabric_eth_network_policy1" {
 # VLANs for the above policy
 # -----------------------------------------------------------------------------
 resource "intersight_fabric_vlan" "fabric_vlan1" {
-  # for_each = var.uplink_vlans_6454
-  for_each = {42,43,1000,1001}
+  # for_each = local.uplink_vlans_6454
+  for_each = {
+    "vlan-42"   = "42"
+    "vlan-43"   = "43"
+    "vlan-1000" = "1000"
+    "vlan-1001" = "1001"
+    }
   auto_allow_on_uplinks = true
   is_native             = false
   name                  = each.key
