8e8955127c735d3db1c241b425b7e89b6b4d8ca6
bywhite <bywhite@cisco.com>
2022-12-13 15:15:08 -0600

    created M2 Storage Policy

--- imm-pod-servers-mod/server-storage.tf
+++ imm-pod-servers-mod/server-storage.tf
@@ -5,3 +5,25 @@
 #  - 
 # -----------------------------------------------------------------------------
 
+resource "intersight_storage_storage_policy" "server_storage_policy1" {
+  name                     = "${var.server_policy_prefix}-vmw-boot-order-policy-1"
+  description              = var.description
+  m2_virtual_drive {
+    enable      = true
+    controller_slot = "MSTOR-RAID-1"
+    object_type = "storage.M2VirtualDriveConfig"
+  }
+  use_jbod_for_vd_creation = false
+  unused_disks_state       = "NoChange"
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+  global_hot_spares = "3"
+  
+}
+
+variable "organization" {
+   type = string
+   description = "<value for organization>"
+ }
