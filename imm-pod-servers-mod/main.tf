653b267b1cec4abbd1b42edad9473fe93560ad43
bywhite <bywhite@cisco.com>
2022-12-13 10:54:46 -0600

    removing object type uuid

--- imm-pod-servers-mod/main.tf
+++ imm-pod-servers-mod/main.tf
@@ -17,7 +17,7 @@ resource "intersight_server_profile_template" "server_template_1" {
   uuid_address_type = "POOL"
   uuid_pool = {
     moid        = var.uuid_pool_moid
-    object_type = "uuidpool.Pool"
+    # object_type = "uuidpool.Pool"
   }
 
 
