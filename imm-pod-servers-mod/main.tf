d0ee845d19147cad302308674776c2306808ba4c
bywhite <bywhite@cisco.com>
2022-12-13 10:25:42 -0600

    adding uuid_pool_name

--- imm-pod-servers-mod/main.tf
+++ imm-pod-servers-mod/main.tf
@@ -16,8 +16,8 @@ resource "intersight_server_profile_template" "server_template_1" {
   target_platform = "FIAttached"
   uuid_address_type = "POOL"
   
-  uuid_pool = {
-    moid        = var.uuid_pool_name
+  uuid_pool_name = {
+    name        = var.uuid_pool_name
     object_type = "uuidpool.Pool"
   } 
 
