64475ab595a5369bc137c122ec3a37a882649a44
bywhite <bywhite@cisco.com>
2022-12-14 15:02:15 -0600

    added M2 storage back into vmw template

--- imm-pod-servers-vmw1-mod/server-storage.tf
+++ imm-pod-servers-vmw1-mod/server-storage.tf
@@ -8,11 +8,12 @@
 resource "intersight_storage_storage_policy" "server_storage_policy1" {
   name                     = "${var.server_policy_prefix}-storage-policy-1"
   description              = var.description
-  # m2_virtual_drive {
-  #   enable      = true
-  #   controller_slot = "MSTOR-RAID-1"
-  #   object_type = "storage.M2VirtualDriveConfig"
-  # }
+  
+  m2_virtual_drive {
+    enable      = true
+    controller_slot = "MSTOR-RAID-1"
+    object_type = "storage.M2VirtualDriveConfig"
+  }
 
   # use_jbod_for_vd_creation = false
   # unused_disks_state       = "NoChange"
