f2b69ad1fd1fd64922c83e19c3217b44b5201d94
bywhite <bywhite@cisco.com>
2022-12-14 21:40:17 -0600

    intersight_vnic_san_connectivity_policy to srv-t

--- imm-pod-servers-vmw1-mod/main.tf
+++ imm-pod-servers-vmw1-mod/main.tf
@@ -77,7 +77,10 @@ resource "intersight_server_profile_template" "server_template_1" {
     object_type = "vnic.LanConnectivityPolicy"
   }
 
-
+  policy_bucket {
+    moid        = intersight_vnic_san_connectivity_policy.vnic_san_con_1.moid
+    object_type = "vnic.SanConnectivityPolicy"
+  }
   
   # IMC User Policy  
   policy_bucket {
@@ -97,6 +100,7 @@ resource "intersight_server_profile_template" "server_template_1" {
   depends_on = [
     intersight_vmedia_policy.vmedia_1, intersight_power_policy.server_power_x, intersight_snmp_policy.snmp1,
     intersight_syslog_policy.syslog_policy, intersight_iam_end_point_user_policy.imc_user1,
-    intersight_bios_policy.bios_default_policy  # intersight_storage_storage_policy.server_storage_policy1,
+    intersight_bios_policy.bios_default_policy, intersight_vnic_san_connectivity_policy.vnic_san_con_1.moid 
+    # intersight_storage_storage_policy.server_storage_policy1,
   ]
 }
