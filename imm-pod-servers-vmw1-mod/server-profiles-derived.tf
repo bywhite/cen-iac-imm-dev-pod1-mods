ab16fad91e8538a88e6a9b565a3d37509b2c0010
bywhite <bywhite@cisco.com>
2022-12-16 18:15:13 -0600

    removing cloner

--- imm-pod-servers-vmw1-mod/server-profiles-derived.tf
+++ imm-pod-servers-vmw1-mod/server-profiles-derived.tf
@@ -6,24 +6,24 @@
 # Care must be taken if making changes to code or variables that will result in the deletion of
 # such resources due to these limitations.
 
-resource "intersight_bulk_mo_cloner" "example" {
-  # this will derive five profiles due to the way the range function works...
-  for_each = toset(formatlist("%s", range(1, 2 + 1)))
+# resource "intersight_bulk_mo_cloner" "example" {
+#   # this will derive five profiles due to the way the range function works...
+#   for_each = toset(formatlist("%s", range(1, 2 + 1)))
 
-  sources {
-    object_type = "server.ProfileTemplate"
-    moid        = intersight_server_profile_template.server_template_1.moid
-  }
+#   sources {
+#     object_type = "server.ProfileTemplate"
+#     moid        = intersight_server_profile_template.server_template_1.moid
+#   }
 
-  targets {
-    object_type = "server.Profile"
-    additional_properties = jsonencode({
-      Name = format("${var.server_policy_prefix}-derived-server-%s", each.key)
-    })
-  }
+#   targets {
+#     object_type = "server.Profile"
+#     additional_properties = jsonencode({
+#       Name = format("${var.server_policy_prefix}-derived-server-%s", each.key)
+#     })
+#   }
 
-  lifecycle {
-    ignore_changes = all # This is required for this resource type
-  }
+#   lifecycle {
+#     ignore_changes = all # This is required for this resource type
+#   }
 
-}
+# }
