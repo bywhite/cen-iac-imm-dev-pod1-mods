0a2727a5bf915a6d52df4f3c29bda77ad95f98ad
bywhite <bywhite@cisco.com>
2022-12-18 13:54:15 -0600

    updated comments

--- imm-pod-servers-vmw-basic-mod/server-profiles-derived.tf
+++ imm-pod-servers-vmw-basic-mod/server-profiles-derived.tf
@@ -1,4 +1,61 @@
 # =============================================================================
+# Server Profiles from locally defined server template
+# -----------------------------------------------------------------------------
+# Reference: https://registry.terraform.io/providers/CiscoDevNet/Intersight/latest/docs/resources/bios_policy
+
+
+resource "intersight_server_profile" "server_profile_list" {
+  # Will add Count to create multiple instances and use index to change server names
+  count = var.server_count
+
+  name                   = "${var.server_policy_prefix}-server-${count.index + 1}"
+  # description          = var.description        # Set by template
+  # action               = "No-op"                # Set by template
+  server_assignment_mode = "None"                 #options: "POOL" "Static" "None"
+  # target_platform      = "FIAttached"           # Set by template
+  type = "instance"
+
+
+  src_template {
+      moid = intersight_server_profile_template.server_template_1.moid
+      object_type = "server.ProfileTemplate"
+    }
+
+  organization {
+    moid        = var.organization
+    object_type = "organization.Organization"
+  }
+
+#   dynamic "associated_server_pool" {
+#     for_each = var.associated_server_pool
+#     content {
+#       moid        = assigned_server.value.moid
+#       object_type = "resourcepool.Pool"
+#     }
+#   }
+
+#   dynamic "assigned_server" {
+#     for_each = var.assigned_server
+#     content {
+#       moid        = assigned_server.value.moid
+#       object_type = assigned_server.value.object_type
+#     }
+#   }
+
+  # dynamic "tags" {
+  #   for_each = var.tags
+  #   content {
+  #     key   = tags.value.key
+  #     value = tags.value.value
+  #   }
+  # }
+
+  depends_on = [
+    intersight_server_profile_template.server_template_1
+  ]
+}
+
+# =============================================================================
 # Create Server Profiles from a Server Template
 #  - Bulk Managed Object Cloner
 #
@@ -74,61 +131,3 @@
 #   ]
 
 # }
-
-# =============================================================================
-# Server Profiles from locally defined server template
-# -----------------------------------------------------------------------------
-# Reference: https://registry.terraform.io/providers/CiscoDevNet/Intersight/latest/docs/resources/bios_policy
-
-
-resource "intersight_server_profile" "server_profile_list" {
-  # Will add Count to create multiple instances and use index to change server names
-  count = var.server_count
-
-  name                   = "${var.server_policy_prefix}-server-${count.index + 1}"
-  # description          = var.description        # Set by template
-  # action               = "No-op"                # Set by template
-  server_assignment_mode = "None"                 #options: "POOL" "Static" "None"
-  # target_platform      = "FIAttached"           # Set by template
-  type = "instance"
-
-
-  src_template {
-      moid = intersight_server_profile_template.server_template_1.moid
-      object_type = "server.ProfileTemplate"
-    }
-
-  organization {
-    moid        = var.organization
-    object_type = "organization.Organization"
-  }
-
-#   dynamic "associated_server_pool" {
-#     for_each = var.associated_server_pool
-#     content {
-#       moid        = assigned_server.value.moid
-#       object_type = "resourcepool.Pool"
-#     }
-#   }
-
-#   dynamic "assigned_server" {
-#     for_each = var.assigned_server
-#     content {
-#       moid        = assigned_server.value.moid
-#       object_type = assigned_server.value.object_type
-#     }
-#   }
-
-  # dynamic "tags" {
-  #   for_each = var.tags
-  #   content {
-  #     key   = tags.value.key
-  #     value = tags.value.value
-  #   }
-  # }
-
-  depends_on = [
-    intersight_server_profile_template.server_template_1
-  ]
-}
-
