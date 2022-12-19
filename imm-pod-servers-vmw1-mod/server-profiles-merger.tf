44cda913cef6d1c55c8140a7f69f4e1f1f219f7d
bywhite <bywhite@cisco.com>
2022-12-19 13:52:08 -0600

    removed quotes from object names

--- imm-pod-servers-vmw1-mod/server-profiles-merger.tf
+++ imm-pod-servers-vmw1-mod/server-profiles-merger.tf
@@ -53,8 +53,8 @@ resource "intersight_bulk_mo_merger" "merge-set-template" {
 
   target_config {
     		SrcTemplate {
-		        "Moid": "639c98e777696e2d31fd5c25",
-		        "ObjectType": "server.ProfileTemplate"
+		        Moid: "639c98e777696e2d31fd5c25",
+		        ObjectType: "server.ProfileTemplate"
             }
   }
 
