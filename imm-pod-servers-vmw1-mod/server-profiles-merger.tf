895f1991c8c5b9f47ceae73cd20da9b22a8455ef
bywhite <bywhite@cisco.com>
2022-12-19 15:10:30 -0600

    cleaned up Server Profile Creation - sorta

--- imm-pod-servers-vmw1-mod/server-profiles-merger.tf
+++ imm-pod-servers-vmw1-mod/server-profiles-merger.tf
@@ -6,39 +6,7 @@
 # Care must be taken if making changes to code or variables that will result in the deletion of
 # such resources due to these limitations.
 
-resource "intersight_bulk_mo_merger" "merge-template-config" {
-  count = var.server_count
-
-  organization {
-    moid        = var.organization
-    object_type = "organization.Organization"
-  }
-
-  sources {
-    object_type = "server.ProfileTemplate"
-    moid        = intersight_server_profile_template.server_template_1.moid
-  }
-
-  targets {
-    object_type = "server.Profile"
-    moid        = intersight_server_profile.server_profile_list[count.index].moid
-   # moid       = intersight_server_profile.server_profile_list[3].moid
-  }
-
-  merge_action = "Replace"
-  #  merge_action = "Merge"
-
-  lifecycle {
-    ignore_changes = all # This is required for this resource type
-  }
-
-  depends_on = [
-    intersight_server_profile_template.server_template_1, intersight_server_profile.server_profile_list
-  ]
-
-}
-
-# resource "intersight_bulk_mo_merger" "merge-set-template" {
+# resource "intersight_bulk_mo_merger" "merge-template-config" {
 #   count = var.server_count
 
 #   organization {
@@ -46,35 +14,25 @@ resource "intersight_bulk_mo_merger" "merge-template-config" {
 #     object_type = "organization.Organization"
 #   }
 
-# #   sources {
-# #     object_type = "server.ProfileTemplate"
-# #     moid        = intersight_server_profile_template.server_template_1.moid
-# #   }
-
-#   target_config {
-#         object_type = "server.Profile"
-#         moid        = intersight_server_profile.server_profile_list[count.index].moid
-#     	SrcTemplate {
-# 		        Moid = "639c98e777696e2d31fd5c25"
-# 		        ObjectType = "server.ProfileTemplate"
-#             }
+#   sources {
+#     object_type = "server.ProfileTemplate"
+#     moid        = intersight_server_profile_template.server_template_1.moid
 #   }
 
-# #   targets {
-# #     object_type = "server.Profile"
-# #     moid        = intersight_server_profile.server_profile_list[count.index].moid
-# #    # moid       = intersight_server_profile.server_profile_list[3].moid
-# #   }
+#   targets {
+#     object_type = "server.Profile"
+#     moid        = intersight_server_profile.server_profile_list[count.index].moid
+#    # moid       = intersight_server_profile.server_profile_list[3].moid
+#   }
 
-#   #merge_action = "Replace"
-#   merge_action = "Merge"
+#   merge_action = "Replace"
+#   #  merge_action = "Merge"
 
 #   lifecycle {
 #     ignore_changes = all # This is required for this resource type
 #   }
 
 #   depends_on = [
-#     intersight_bulk_mo_merger.merge-template-config
+#     intersight_server_profile_template.server_template_1, intersight_server_profile.server_profile_list
 #   ]
-
 # }
--- imm-pod-servers-vmw1-mod/server-profiles-sorta.tf
+++ imm-pod-servers-vmw1-mod/server-profiles-sorta.tf
@@ -49,9 +49,35 @@ resource "intersight_server_profile" "server_profile_list" {
 #     }
 #   }
 
-# Initially we will create the Server Profiles separate from the Template
-  # depends_on = [
-  #   intersight_server_profile_template.server_template_1
-  # ]
+  depends_on = [
+    intersight_server_profile_template.server_template_1
+  ]
 }
 
+
+# This sets the initial values of the Server Profile to match the un-attached server profile template
+# There is currently an issue with the Terraform Provider that prohibits proper Template/Profile behavior
+resource "intersight_bulk_mo_merger" "merge-template-config" {
+  count = var.server_count
+  organization {
+    moid        = var.organization
+    object_type = "organization.Organization"
+  }
+  sources {
+    object_type = "server.ProfileTemplate"
+    moid        = intersight_server_profile_template.server_template_1.moid
+  }
+  targets {
+    object_type = "server.Profile"
+    moid        = intersight_server_profile.server_profile_list[count.index].moid
+  }
+  merge_action = "Replace"
+  #  merge_action = "Merge"
+  lifecycle {
+    ignore_changes = all # This is required for this resource type
+  }
+  depends_on = [
+    intersight_server_profile_template.server_template_1, intersight_server_profile.server_profile_list
+  ]
+
+}
