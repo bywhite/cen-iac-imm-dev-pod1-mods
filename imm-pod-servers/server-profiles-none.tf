60bfba318b4104dd7cab51883ac9d46fa69bf8f2
bywhite <bywhite@cisco.com>
2022-12-20 11:02:16 -0600

    duplicated vmw mod to servers mod

---
+++ imm-pod-servers/server-profiles-none.tf
@@ -0,0 +1,75 @@
+# =============================================================================
+# Create Server Profiles from a Server Template
+#  - Bulk Managed Object Cloner
+#
+# This resource has significant limitations, but it does work.
+# It can be used to create profiles that are attached to templates, but it cannot delete them.
+# Terraform will delete them from the state but also warn that they cannot be deleted from Intersight.
+# This resource cannot tell if the created profiles have actually been deleted due to the (necessary) lifecycle block.
+# Care must be taken if making changes to code or variables that will result in the deletion of
+# such resources due to these limitations.
+# -----------------------------------------------------------------------------
+
+# resource "intersight_bulk_mo_cloner" "server_profile_clones" {
+#   # this will derive five profiles due to the way the range function works...
+#   for_each = toset(formatlist("%s", range(1, var.server_count + 1)))
+
+#   sources {
+#     object_type = "server.ProfileTemplate"
+#     moid        = intersight_server_profile_template.server_template_1.moid
+#   }
+
+#   targets {
+#     object_type = "server.Profile"
+#     additional_properties = jsonencode({
+#       Name = format("${var.server_policy_prefix}-server-%s", each.key)
+#     })
+#   }
+
+#   lifecycle {
+#     ignore_changes = all # This is required for this resource type
+#   }
+
+# }
+
+
+
+#### Alternative method to derive server profiles is to create and then Merge server template values
+
+# This resource has significant limitations, but it does work.
+# It can be used to create profiles that are attached to templates, but it cannot delete them.
+# Terraform will delete them from the state but also warn that they cannot be deleted from Intersight.
+# This resource cannot tell if the created profiles have actually been deleted due to the (necessary) lifecycle block.
+# Care must be taken if making changes to code or variables that will result in the deletion of
+# such resources due to these limitations.
+
+# resource "intersight_bulk_mo_merger" "merge-server-config" {
+#   count = var.server_count
+
+#   organization {
+#     moid        = var.organization
+#     object_type = "organization.Organization"
+#   }
+
+#   sources {
+#     object_type = "server.ProfileTemplate"
+#     moid        = intersight_server_profile_template.server_template_1.moid
+#   }
+
+#   targets {
+#     object_type = "server.Profile"
+#     moid        = intersight_server_profile.server_profile_list[count.index].moid
+#    # moid       = intersight_server_profile.server_profile_list[3].moid
+#   }
+
+#   merge_action = "Merge"
+
+#   lifecycle {
+#     ignore_changes = all # This is required for this resource type
+#   }
+
+#   depends_on = [
+#     intersight_server_profile_template.server_template_1
+#   ]
+
+# }
