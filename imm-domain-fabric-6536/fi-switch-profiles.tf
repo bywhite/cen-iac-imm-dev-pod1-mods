f72f3cfac9e6befa38d62e096b567df604db594e
bywhite <bywhite@cisco.com>
2022-12-20 11:41:22 -0600

    testing pod qos on switch b

--- imm-domain-fabric-6536/fi-switch-profiles.tf
+++ imm-domain-fabric-6536/fi-switch-profiles.tf
@@ -50,16 +50,16 @@ resource "intersight_fabric_switch_profile" "fi6536_switch_profile_b" {
   switch_cluster_profile {
     moid = intersight_fabric_switch_cluster_profile.fi6536_cluster_profile.moid
   }
-  policy_bucket {
-    moid        = intersight_fabric_system_qos_policy.qos1.moid
-    object_type = "fabric.SystemQosPolicy"
-  }
-
-
-  # policy_bucket = {
-  #   moid        = pod_qos_moid
+  # policy_bucket {
+  #   moid        = intersight_fabric_system_qos_policy.qos1.moid
   #   object_type = "fabric.SystemQosPolicy"
   # }
+
+
+  policy_bucket = {
+    moid        = pod_qos_moid
+    object_type = "fabric.SystemQosPolicy"
+  }
   dynamic "tags" {
     for_each = var.tags
     content {
