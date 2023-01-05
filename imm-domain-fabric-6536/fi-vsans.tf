32150fcc1676789e2c3ce39047204f4a4698567e
bywhite <bywhite@cisco.com>
2023-01-05 16:44:22 -0600

    enabled vsan parameters

--- imm-domain-fabric-6536/fi-vsans.tf
+++ imm-domain-fabric-6536/fi-vsans.tf
@@ -50,54 +50,84 @@ resource "intersight_fabric_fc_network_policy" "fabric_fc_network_policy_b" {
 }
 
 
-resource "intersight_fabric_vsan" "fabric_vsan_a_100" {
-  name                  = "${var.policy_prefix}-vsan-a-100"
+resource "intersight_fabric_vsan" "fabric_vsan_a" {
+  for_each = var.fc_6536_vsans_a
+  name                  = "${var.policy_prefix}-vsan-a-${each.value["vsan_a"]}"
   default_zoning        = "Disabled"
   #fc_zone_sharing_mode = "Active"
   #fc_zone_sharing_mode = ""
   vsan_scope            = "Uplink"
-  fcoe_vlan             = 1100
-  vsan_id               = 100
+  fcoe_vlan             = each.value["fcoe_a_vlan"]
+  vsan_id               = each.value["vsan_a"]
   fc_network_policy {
     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
   }
 }
 
-resource "intersight_fabric_vsan" "fabric_vsan_a_101" {
-  name                  = "${var.policy_prefix}-vsan-a-101"
-  default_zoning        = "Disabled"
-  #fc_zone_sharing_mode = "Active"
-  #fc_zone_sharing_mode = ""
-  vsan_scope            = "Uplink"
-  fcoe_vlan             = 1101
-  vsan_id               = 101
-  fc_network_policy {
-    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
-  }
-}
+# resource "intersight_fabric_vsan" "fabric_vsan_a_100" {
+#   name                  = "${var.policy_prefix}-vsan-a-100"
+#   default_zoning        = "Disabled"
+#   #fc_zone_sharing_mode = "Active"
+#   #fc_zone_sharing_mode = ""
+#   vsan_scope            = "Uplink"
+#   fcoe_vlan             = 1100
+#   vsan_id               = 100
+#   fc_network_policy {
+#     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
+#   }
+# }
 
-resource "intersight_fabric_vsan" "fabric_vsan_b_200" {
-  name                  = "${var.policy_prefix}-vsan-b-200"
+# resource "intersight_fabric_vsan" "fabric_vsan_a_101" {
+#   name                  = "${var.policy_prefix}-vsan-a-101"
+#   default_zoning        = "Disabled"
+#   #fc_zone_sharing_mode = "Active"
+#   #fc_zone_sharing_mode = ""
+#   vsan_scope            = "Uplink"
+#   fcoe_vlan             = 1101
+#   vsan_id               = 101
+#   fc_network_policy {
+#     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
+#   }
+# }
+
+
+
+resource "intersight_fabric_vsan" "fabric_vsan_b" {
+  for_each = var.fc_6536_vsans_b
+  name                  = "${var.policy_prefix}-vsan-b-${each.value["vsan_b"]}"
   default_zoning        = "Disabled"
   #fc_zone_sharing_mode = "Active"
   #fc_zone_sharing_mode = ""
   vsan_scope            = "Uplink"
-  fcoe_vlan             = 1200
-  vsan_id               = 200
+  fcoe_vlan             = each.value["fcoe_b_vlan"]
+  vsan_id               = each.value["vsan_b"]
   fc_network_policy {
     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
   }
 }
 
-resource "intersight_fabric_vsan" "fabric_vsan_b_201" {
-  name                  = "${var.policy_prefix}-vsan-b-201"
-  default_zoning        = "Disabled"
-  #fc_zone_sharing_mode = "Active"
-  #fc_zone_sharing_mode = ""
-  vsan_scope            = "Uplink"
-  fcoe_vlan             = 1201
-  vsan_id               = 201
-  fc_network_policy {
-    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
-  }
-}
+# resource "intersight_fabric_vsan" "fabric_vsan_b_200" {
+#   name                  = "${var.policy_prefix}-vsan-b-200"
+#   default_zoning        = "Disabled"
+#   #fc_zone_sharing_mode = "Active"
+#   #fc_zone_sharing_mode = ""
+#   vsan_scope            = "Uplink"
+#   fcoe_vlan             = 1200
+#   vsan_id               = 200
+#   fc_network_policy {
+#     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
+#   }
+# }
+
+# resource "intersight_fabric_vsan" "fabric_vsan_b_201" {
+#   name                  = "${var.policy_prefix}-vsan-b-201"
+#   default_zoning        = "Disabled"
+#   #fc_zone_sharing_mode = "Active"
+#   #fc_zone_sharing_mode = ""
+#   vsan_scope            = "Uplink"
+#   fcoe_vlan             = 1201
+#   vsan_id               = 201
+#   fc_network_policy {
+#     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
+#   }
+# }
