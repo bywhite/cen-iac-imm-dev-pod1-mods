ae223b1c6a5cd5784bf30aafbf0f66689b86dd07
bywhite <bywhite@cisco.com>
2023-01-05 16:19:16 -0600

    Updated FI VSANs

--- imm-domain-fabric-6536/fi-vsans.tf
+++ imm-domain-fabric-6536/fi-vsans.tf
@@ -1,5 +1,6 @@
 # =============================================================================
-# VSAN Policy Assigned to Switch Profiles
+# A Switch Profiles is assigned to each VSAN Policy
+# Policy bucket limitations prevent the reverse of assignment.
 # -----------------------------------------------------------------------------
 
 
@@ -8,12 +9,12 @@ resource "intersight_fabric_fc_network_policy" "fabric_fc_network_policy_a" {
   description     = "${var.description} FC network policy"
   enable_trunking = true
   organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
+    object_type   = "organization.Organization"
+    moid          = var.organization
   }
   profiles {
-    moid        = intersight_fabric_switch_profile.fi6536_switch_profile_a.moid
-    object_type = "fabric.SwitchProfile"
+    moid          = intersight_fabric_switch_profile.fi6536_switch_profile_a.moid
+    object_type   = "fabric.SwitchProfile"
     }
 
   dynamic "tags" {
@@ -25,29 +26,13 @@ resource "intersight_fabric_fc_network_policy" "fabric_fc_network_policy_a" {
   }
 }
 
-resource "intersight_fabric_vsan" "fabric_vsan_a" {
-  name                 = "${var.policy_prefix}-vsan-a-1"
-  default_zoning       = "Disabled"
-  #fc_zone_sharing_mode = "Active"
-  #fc_zone_sharing_mode = ""
-  vsan_scope = "Uplink"
-  
-  # will use each of fc_6536_vsans_a
-  fcoe_vlan            = 1100
-  vsan_id              = 100
-  fc_network_policy {
-    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
-  }
-}
-
-
 resource "intersight_fabric_fc_network_policy" "fabric_fc_network_policy_b" {
   name            = "${var.policy_prefix}-vsan-b-1"
   description     = "${var.description} FC network policy"
   enable_trunking = true
   organization {
-    object_type = "organization.Organization"
-    moid        = var.organization
+    object_type   = "organization.Organization"
+    moid          = var.organization
   }
 
   profiles {
@@ -64,16 +49,54 @@ resource "intersight_fabric_fc_network_policy" "fabric_fc_network_policy_b" {
 
 }
 
-resource "intersight_fabric_vsan" "fabric_vsan_b" {
-  name                 = "${var.policy_prefix}-vsan-b-1"
-  default_zoning       = "Disabled"
+
+resource "intersight_fabric_vsan" "fabric_vsan_a_100" {
+  name                  = "${var.policy_prefix}-vsan-a-100"
+  default_zoning        = "Disabled"
   #fc_zone_sharing_mode = "Active"
   #fc_zone_sharing_mode = ""
-  vsan_scope = "Uplink"
-  # will use each of fc_6536_vsans_b
+  vsan_scope            = "Uplink"
+  fcoe_vlan             = 1100
+  vsan_id               = 100
+  fc_network_policy {
+    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
+  }
+}
 
-  fcoe_vlan            = 1200
-  vsan_id              = 200
+resource "intersight_fabric_vsan" "fabric_vsan_a_101" {
+  name                  = "${var.policy_prefix}-vsan-a-101"
+  default_zoning        = "Disabled"
+  #fc_zone_sharing_mode = "Active"
+  #fc_zone_sharing_mode = ""
+  vsan_scope            = "Uplink"
+  fcoe_vlan             = 1101
+  vsan_id               = 101
+  fc_network_policy {
+    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_a.id
+  }
+}
+
+resource "intersight_fabric_vsan" "fabric_vsan_b_200" {
+  name                  = "${var.policy_prefix}-vsan-b-200"
+  default_zoning        = "Disabled"
+  #fc_zone_sharing_mode = "Active"
+  #fc_zone_sharing_mode = ""
+  vsan_scope            = "Uplink"
+  fcoe_vlan             = 1200
+  vsan_id               = 200
+  fc_network_policy {
+    moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
+  }
+}
+
+resource "intersight_fabric_vsan" "fabric_vsan_b_201" {
+  name                  = "${var.policy_prefix}-vsan-b-201"
+  default_zoning        = "Disabled"
+  #fc_zone_sharing_mode = "Active"
+  #fc_zone_sharing_mode = ""
+  vsan_scope            = "Uplink"
+  fcoe_vlan             = 1201
+  vsan_id               = 201
   fc_network_policy {
     moid = intersight_fabric_fc_network_policy.fabric_fc_network_policy_b.id
   }
