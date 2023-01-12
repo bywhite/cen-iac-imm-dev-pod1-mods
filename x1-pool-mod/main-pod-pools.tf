e8aa7cb182f45bd46f26092879a1c54e9e66b9f5
bywhite <bywhite@cisco.com>
2023-01-12 10:06:58 -0600

    updated Org

--- x1-pool-mod/main-pod-pools.tf
+++ x1-pool-mod/main-pod-pools.tf
@@ -5,8 +5,8 @@ resource "intersight_macpool_pool" "cisco_af_1" {
     from = "00:25:B5:${var.pod_id}:00:01"
     size = 1000
   }
-    organization {
+  organization {
     object_type = "organization.Organization"
     moid = var.organization 
-    }
+  }
 }
