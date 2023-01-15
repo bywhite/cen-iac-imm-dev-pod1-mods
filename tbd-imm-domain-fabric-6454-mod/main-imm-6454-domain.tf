94b8df852897feec42a85b9f4706e82ac3859b76
bywhite <bywhite@cisco.com>
2023-01-14 19:05:06 -0600

    6454 domain mod update

--- tbd-imm-domain-fabric-6454-mod/main-imm-6454-domain.tf
+++ tbd-imm-domain-fabric-6454-mod/main-imm-6454-domain.tf
@@ -1,21 +1,18 @@
 # =============================================================================
-# IMM Cluster (Domain) and Switch Profiles for 6454 Fabric Interconnects
+# This main module creates a 6454 Cluster (Domain) Profile
+# Used by: switch profile's self-assignment to this cluster profile
 # -----------------------------------------------------------------------------
 
-
-
-# =============================================================================
-# 6454 Cluster (Domain) and Switch Profiles
-# -----------------------------------------------------------------------------
-
-resource "intersight_fabric_switch_cluster_profile" "fabric_switch_cluster_profile1" {
-  name        = "${var.policy_prefix}-fi6454-domain-profile-1"
+### FI 6454 DOMAIN Profile  (cluster_profile) ####
+resource "intersight_fabric_switch_cluster_profile" "fi6454_cluster_profile" {
+  name        = "${var.policy_prefix}-imm-domain"
   description = var.description
   type        = "instance"
   organization {
     object_type = "organization.Organization"
     moid        = var.organization
   }
+
   dynamic "tags" {
     for_each = var.tags
     content {
@@ -23,73 +20,8 @@ resource "intersight_fabric_switch_cluster_profile" "fabric_switch_cluster_profi
       value = tags.value.value
     }
   }
-}
-
-
-# =============================================================================
-# Switches (A and B)
-# -----------------------------------------------------------------------------
-
-resource "intersight_fabric_switch_profile" "fi6454_switch_profile_a" {
-  action      = "No-op"
-  description = var.description
-  name        = "${var.policy_prefix}-fi6454-a-profile-1"
-  type        = "instance"
-  switch_cluster_profile {
-    moid = intersight_fabric_switch_cluster_profile.fabric_switch_cluster_profile1.moid
-  }
-  dynamic "tags" {
-    for_each = var.tags
-    content {
-      key   = tags.value.key
-      value = tags.value.value
-    }
-  }
-
-  # policy_bucket {
-  #   moid = intersight_fabric_eth_network_policy.fabric_eth_network_policy1.moid
-  #   object_type = "fabric.EthNetworkPolicy"
-  # }
-  # policy_bucket {
-  #   moid = intersight_fabric_port_policy.fi6454_port_policy1.moid
-  #   object_type = "fabric.PortPolicy"
-  # }
-  # policy_bucket {
-  #   moid = intersight_ntp_policy.ntp1.moid
-  #   object_type = "ntp.Policy"
-  # }
-}
-resource "intersight_fabric_switch_profile" "fi6454_switch_profile_b" {
-  action      = "No-op"
-  description = var.description
-  name        = "${var.policy_prefix}-fi6454-b-profile-1"
-  type        = "instance"
-  switch_cluster_profile {
-    moid = intersight_fabric_switch_cluster_profile.fabric_switch_cluster_profile1.moid
-  }
-  dynamic "tags" {
-    for_each = var.tags
-    content {
-      key   = tags.value.key
-      value = tags.value.value
-    }
-  }
-
-  # policy_bucket {
-  #   moid = intersight_fabric_eth_network_policy.fabric_eth_network_policy1.moid
-  #   object_type = "fabric.EthNetworkPolicy"
-  # }
-  # policy_bucket {
-  #   moid = intersight_fabric_port_policy.fi6454_port_policy1.moid
-  #   object_type = "fabric.PortPolicy"
-  # }
-  # policy_bucket {
-  #   moid = intersight_ntp_policy.ntp1.moid
-  #   object_type = "ntp.Policy"
-  # }
 }
 
 # =============================================================================
-# END of 6456 Switch Cluster (Domain) Profile
+# END of 6454 Switch Cluster (Domain) Profile
 # -----------------------------------------------------------------------------
-
