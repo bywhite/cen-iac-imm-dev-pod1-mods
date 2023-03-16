cf796bee4bb5da77ddcb49998683643b6e3c8421
bywhite <bywhite@cisco.com>
2023-03-16 10:55:43 -0500

    cleaned up 6454 formatting

--- imm-domain-fabric-6454-mod/variables.tf
+++ imm-domain-fabric-6454-mod/variables.tf
@@ -34,24 +34,24 @@ variable "tags" {
 # Fabric Interconnect 6454 ports and VLANs
 # -----------------------------------------------------------------------------
 
-variable "server_ports_6454" {
-  type        = set(string)
-  description = "list of port numbers to be assigned to server ports"
-}
 variable "port_channel_6454" {
   type        = set(string)
   description = "list of ethernet port numbers to be assigned to uplink port channel"
 }
-
-variable "switch_vlans_6454" {
-  type        = string
-  description = "comma separated vlans and/or vlan ranges Ex: 5,6,7,8,100-130,998-1011"
+variable "server_ports_6454" {
+  type        = set(string)
+  description = "list of port numbers to be assigned to server ports"
 }
 variable "vlan_prefix" {
   type        = string
   description = "prepended to vlan-id    EX:   vlan-123"
   default = "vlan"
 }
+variable "switch_vlans_6454" {
+  type        = string
+  description = "comma separated vlans and/or vlan ranges Ex: 5,6,7,8,100-130,998-1011"
+}
+
 
 # =============================================================================
 # Fabric Interconnect 6454 SAN ports and VSANs
@@ -62,10 +62,7 @@ variable "fc_port_count_6454" {
   default     = 0
 }
 
-variable "fc_port_channel_6454" {
-  type        = list (map(number))
-  default     = []
-}
+
 variable "fc_uplink_pc_vsan_id_a" {
   type        = number
   default     = 1
@@ -73,6 +70,10 @@ variable "fc_uplink_pc_vsan_id_a" {
 variable "fc_uplink_pc_vsan_id_b" {
   type        = number
   default     = 1
+}
+variable "fc_port_channel_6454" {
+  type        = list (map(number))
+  default     = []
 }
 
 variable "fabric_a_vsan_sets" {
@@ -136,11 +137,12 @@ variable "dns_alternate" {
   description = "IP address of secondary (alternate) DNS server"
   default     = ""
 }
-variable "snmp_password" {
-  type        = string
-  default     = "Cisco123"
-}
+
 variable "snmp_ip"  {
   type        = string
   default     = "127.0.0.1"
+}
+variable "snmp_password" {
+  type        = string
+  default     = "Cisco123"
 }
