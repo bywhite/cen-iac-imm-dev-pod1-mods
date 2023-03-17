b697275e6e7bfbe48a3d225c0b90c68b44655153
bywhite <bywhite@cisco.com>
2023-03-17 14:10:12 -0500

    fixed chassis count index

--- imm-domain-fabric-6454-mod/locals.tf
+++ imm-domain-fabric-6454-mod/locals.tf
@@ -1,8 +1,8 @@
 
 locals {
 
-  # Create a list of chassis indexes Example of five chassis: [0,1,2,3,4]
-  chassis_index_numbers  = range(1,var.chassis_9508_count)
+  # Create a list of chassis indexes Example of five chassis: [1,2,3,4,5]
+  chassis_index_numbers  = range(1,var.chassis_9508_count + 1)
 
   # Convert the list of numbers to a set of strings
   chassis_index_set     = toset([for v in local.chassis_index_numbers : tostring(v)])
