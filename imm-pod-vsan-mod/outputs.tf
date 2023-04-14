568d69199930bd12f192edb758fcada1b120b2e2
bywhite <bywhite@cisco.com>
2023-04-14 16:31:51 -0500

    Updated FC SAN Module

---
+++ imm-pod-vsan-mod/outputs.tf
@@ -0,0 +1,34 @@
+# This module returns the Intersight IAM End Point User Policy Moid
+# for use in the Server Profile Template for IMC access
+
+
+output "fc_vsan_100_moid" {
+  description = "Pod MOID for VSAN 100"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_100.moid
+}
+
+output "fc_vsan_101_moid" {
+  description = "Pod MOID for VSAN 101"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_101.moid
+}
+
+output "fc_vsan_102_moid" {
+  description = "Pod MOID for VSAN 102"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_102.moid
+}
+
+output "fc_vsan_200_moid" {
+  description = "Pod MOID for VSAN 200"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_200.moid
+}
+
+output "fc_vsan_201_moid" {
+  description = "Pod MOID for VSAN 201"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_201.moid
+}
+
+output "fc_vsan_202_moid" {
+  description = "Pod MOID for VSAN 202"
+  value       = intersight_vnic_fc_network_policy.fc_vsan_202.moid
+}
+
---
+++ imm-pod-vsan-mod/variables.tf
@@ -0,0 +1,13 @@
+
+variable "organization" {
+  type = string
+}
+
+variable "pod_policy_prefix" {
+  type = string
+}
+
+variable "description" {
+  type        = string
+  default = "Pod IMC Default Policy"
+}
