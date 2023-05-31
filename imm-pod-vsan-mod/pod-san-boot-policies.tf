a451e70511675ac95786f12ccb5fc50cce9ebee6
bywhite <bywhite@cisco.com>
2023-05-31 16:02:35 -0500

    Creating 1 SAN Boot Policy for Pod

---
+++ imm-pod-vsan-mod/pod-san-boot-policies.tf
@@ -0,0 +1,76 @@
+# =============================================================================
+# Server Precision Boot Order Policies for FI-Attached server template
+# Creates "Boot Order Policy"
+# Examples: https://github.com/terraform-cisco-modules/terraform-intersight-imm/blob/master/examples/policies/boot_order_policies.tf
+# -----------------------------------------------------------------------------
+
+resource "intersight_boot_precision_policy" "san_boot_policies" {
+  name                     = "${var.pod_policy_prefix}-san-boot"
+  description              = "${var.pod_policy_prefix} SAN Boot Policies"
+  configured_boot_mode     = "Uefi"
+  enforce_uefi_secure_boot = false
+  organization {
+    moid        = var.organization
+    object_type = "organization.Organization"
+  }
+
+  boot_devices {
+    enabled     = true
+    name        = "KVM_DVD"
+    object_type = "boot.VirtualMedia"
+     additional_properties = jsonencode({
+       Subtype = "kvm-mapped-dvd"
+    })
+  }
+
+  # boot_devices {
+  #   enabled     = true
+  #   name        = "IMC_DVD"
+  #   object_type = "boot.VirtualMedia"
+  #   additional_properties = jsonencode({
+  #     Subtype = "cimc-mapped-dvd"
+  #   })
+  # }
+
+  # boot_devices {
+  #   enabled     = true
+  #   name        = "M2_Boot"
+  #   object_type = "boot.LocalDisk"
+  #   additional_properties = jsonencode({
+  #     Slot    = "MSTOR-RAID"
+  #     Bootloader = {
+  #       Description = "M2 Boot"
+  #       Name        = "BOOTX64.EFI"
+  #       ObjectType  = "boot.Bootloader"
+  #       Path        = "\\EFI\\BOOT\\"
+  #     }
+  #   })
+  # }
+
+  # boot_devices {
+  #   enabled     = true
+  #   name        = "RAID_Boot"
+  #   object_type = "boot.LocalDisk"
+  #   additional_properties = jsonencode({
+  #     Slot       = "MRAID"
+  #     Bootloader = {
+  #       Description = "RAID Boot"
+  #       Name        = "BOOTX64.EFI"
+  #       ObjectType  = "boot.Bootloader"
+  #       Path        = "\\EFI\\BOOT\\"
+  #     }
+  #   })
+  # }
+
+  # boot_devices {
+  #   enabled         = true
+  #   name            = "PXE"
+  #   object_type     = "boot.Pxe"
+  #   additional_properties = jsonencode({
+  #     Slot            = "MLOM"
+  #     InterfaceSource = "name"
+  #     InterfaceName   = "eth0"
+  #     IpType          = "IPv4"
+  #   })
+  
+  }
--- imm-pod-vsan-mod/variables.tf
+++ imm-pod-vsan-mod/variables.tf
@@ -9,5 +9,28 @@ variable "pod_policy_prefix" {
 
 variable "description" {
   type        = string
-  default = "Pod IMC Default Policy"
+  default     = "Pod IMC Default Policy"
 }
+
+
+variable "san_boot_targets" {
+  type       = map(object({
+    interface_name = string
+    boot_lun       = number
+    target_wwpn    = string
+  }))
+  description = "Map of vNic interfaces paired with their vlan range"
+  default = {
+    "fc0" = {
+      interface_name = "fc0"
+      boot_lun       = 0
+      target_wwpn    = "00:00:00:00:00:00:00:00"
+    }
+    "fc1" = {
+      interface_name = "fc1"
+      boot_lun       = 0
+      target_wwpn    = "00:00:00:00:00:00:00:00"
+    }
+  }
+}
+# Usage: for_each var.vsan_boot_targets  each.value["interface_name"]  each.value["boot_lun"]  each.value["target_wwpn"]
