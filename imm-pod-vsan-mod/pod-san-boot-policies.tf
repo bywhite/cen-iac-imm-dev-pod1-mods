fce4afc42d305c54fa807e406cf94868cc7a3f41
bywhite <bywhite@cisco.com>
2023-06-01 10:51:17 -0500

    Add Common SAN boot policies

--- imm-pod-vsan-mod/pod-san-boot-policies.tf
+++ imm-pod-vsan-mod/pod-san-boot-policies.tf
@@ -4,8 +4,11 @@
 # Examples: https://github.com/terraform-cisco-modules/terraform-intersight-imm/blob/master/examples/policies/boot_order_policies.tf
 # -----------------------------------------------------------------------------
 
+
 resource "intersight_boot_precision_policy" "san_boot_policies" {
-  name                     = "${var.pod_policy_prefix}-san-boot"
+  for_each = var.vsan_boot_targets
+  # Usage: for_each var.vsan_boot_targets  each.value["int_name_1"]  each.value["boot_lun_1"]  each.value["target_wwpn_1"]
+  name                     = "${var.pod_policy_prefix}-san-${each.key}"
   description              = "${var.pod_policy_prefix} SAN Boot Policies"
   configured_boot_mode     = "Uefi"
   enforce_uefi_secure_boot = false
@@ -25,51 +28,52 @@ resource "intersight_boot_precision_policy" "san_boot_policies" {
 
   boot_devices {
     enabled     = true
-    name        = "fc0"
+    name        = each.value["int_name_1"] 
     object_type = "boot.San"
     additional_properties = jsonencode({
       Slot          = "MLOM"
-      InterfaceName = "fc0"
-      Lun           = 0
-      Wwpn          = "50:0A:09:82:89:0B:9B:0A"
+      InterfaceName = each.value["int_name_1"] 
+      Lun           = each.value["boot_lun_1"] 
+      Wwpn          = each.value["target_wwpn_1"] 
     })
   }
 
   boot_devices {
     enabled     = true
-    name        = "fc1"
+    name        = each.value["int_name_2"] 
     object_type = "boot.San"
     additional_properties = jsonencode({
       Slot          = "MLOM"
-      InterfaceName = "fc1"
-      Lun           = 0
-      Wwpn          = "50:0A:09:82:89:0B:9B:0B"
-    })
-  }
-
-    boot_devices {
-    enabled     = true
-    name        = "fc2"
-    object_type = "boot.San"
-    additional_properties = jsonencode({
-      Slot          = "MLOM"
-      InterfaceName = "fc2"
-      Lun           = 0
-      Wwpn          = "50:0A:09:82:89:0B:9B:0C"
+      InterfaceName = each.value["int_name_2"] 
+      Lun           = each.value["boot_lun_2"] 
+      Wwpn          = each.value["target_wwpn_2"] 
     })
   }
 
   boot_devices {
     enabled     = true
-    name        = "fc3"
+    name        = each.value["int_name_3"] 
     object_type = "boot.San"
     additional_properties = jsonencode({
       Slot          = "MLOM"
-      InterfaceName = "fc3"
-      Lun           = 0
-      Wwpn          = "50:0A:09:82:89:0B:9B:0D"
+      InterfaceName = each.value["int_name_3"] 
+      Lun           = each.value["boot_lun_3"] 
+      Wwpn          = each.value["target_wwpn_3"] 
     })
   }
+
+  boot_devices {
+    enabled     = true
+    name        = each.value["int_name_4"] 
+    object_type = "boot.San"
+    additional_properties = jsonencode({
+      Slot          = "MLOM"
+      InterfaceName = each.value["int_name_4"] 
+      Lun           = each.value["boot_lun_4"] 
+      Wwpn          = each.value["target_wwpn_4"] 
+    })
+  }
+
 
   # boot_devices {
   #   enabled     = true
