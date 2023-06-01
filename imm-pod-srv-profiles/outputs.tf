c6f90c0bf0cfc6e36151a6addb05d6009fa1cf14
bywhite <bywhite@cisco.com>
2023-06-01 15:02:59 -0500

    remove outputs

--- imm-pod-srv-profiles/outputs.tf
+++ imm-pod-srv-profiles/outputs.tf
@@ -3,17 +3,17 @@
 # -----------------------------------------------------------------------------
 
 
-output "server_profile_names" {
-  description = "Names of the created server profiles"
-  value       = intersight_server_profile.server_profiles.name
-}
+# output "server_profile_names" {
+#   description = "Names of the created server profiles"
+#   value       = intersight_server_profile.server_profiles.name
+# }
 
-output "server_profile_moids" {
-  description = "MOID of the created server profile template"
-  value       = intersight_server_profile.server_profiles.moid
-}
+# output "server_profile_moids" {
+#   description = "MOID of the created server profile template"
+#   value       = intersight_server_profile.server_profiles.moid
+# }
 
-output "server_interfaces" {
-  description = "List of created interfaces"
-  value       = intersight_fabric_eth_network_group_policy.fabric_eth_network_group_policy1
-}
+# output "server_interfaces" {
+#   description = "List of created interfaces"
+#   value       = intersight_fabric_eth_network_group_policy.fabric_eth_network_group_policy1
+# }
